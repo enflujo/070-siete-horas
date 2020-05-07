9073b15cbac082af0206a8a7768172d346977790
Juan Camilo González Jiménez <info@juancgonzalez.com>
2020-05-07 09:57:24 -0500

    probar menu UI

---
+++ src/components/Menu.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav id="nav">
+    <ul>
+      <li>Home</li>
+      <li>Autoplay</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Menu'
+};
+</script>
+
+<style scoped lang="scss">
+@import '../scss/utils/_variables.scss';
+
+#nav {
+  width: $menuWidth;
+  height: $viewHeight;
+  // position: fixed;
+  // left: 0;
+  // top: 0;
+  background-color: yellow;
+}
+</style>
